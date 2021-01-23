c7f0f6bd713a6f160828eb3116433d71ea479032
DucNhu <60964371+DucNhu@users.noreply.github.com>
2021-01-23 14:11:23 +0700

    Create Project

---
+++ MADAM_Boutique/MADAM_Boutique/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MADAM_Boutique.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
